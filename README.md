0601f935edb86dd4fecb62b1abcbca089fd00f01
Ewerton Coutinho <ebcoutinho91@gmail.com>
2026-02-02 09:26:00 -0300

    adiciona scripts 'lint:chek' e 'lint:fix'

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para aprimoramento de habilidades enquanto desenvolvedor web
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Feliz 2026! Deus é bom!!! 😎</h1>
+  return <h1>Feliz 2026! Deus é bom!!! 😎</h1>;
+}
+
+function print() {
+  console.log("Olá, mundo!");
 }
 
 export default Home;
