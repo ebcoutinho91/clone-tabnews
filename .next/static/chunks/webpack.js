0c877a2bbff848993c85ca3f8480410eef4d88ac
Ewerton Coutinho <ebcoutinho91@gmail.com>
2026-02-12 10:42:38 -0300

    adiciona scripts test e test:watch'

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9f06a3fc0dcf229c"; }
+/******/ 		__webpack_require__.h = function() { return "58eb6b16ff2a0828"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
